8e1c1c91ed4d4f8c1fd570174dcf14bdc0eb857f
ebruskokos <120543798+ebruskokos@users.noreply.github.com>
2023-07-13 16:02:17 -0500

    terraform_eks

---
+++ terraform/0-provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  requirequired_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+provider "aws" {
+  region = "us-east-1"
+
+}
+
+variable "cluster_name" {
+  default = "demo"
+}
+
+variable "cluster_version" {
+  default = "1.22"
+
+}
